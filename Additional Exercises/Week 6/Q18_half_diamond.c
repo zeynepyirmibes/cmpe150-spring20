a2387df6413f92e1620b06908bb04cbb1b45d6fc
zeynepyirmibes <60973522+zeynepyirmibes@users.noreply.github.com>
2020-04-13 20:14:48 +0300

    Question 18 added

---
+++ Additional Exercises/Week 6/Q18_half_diamond.c
@@ -0,0 +1,37 @@
+/* Write a C program to print a half diamond star pattern using for loop. */
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j, N, columns;
+
+    /* Input number of columns from user */
+    scanf("%d",&N);
+
+    columns=1;
+
+    for(i=1;i<N*2;i++)
+    {
+        for(j=1; j<=columns; j++)
+        {
+            printf("*");
+        }
+
+        if(i < N)
+        {
+            /* Increment number of columns per row for upper part */
+            columns++;
+        }
+        else
+        {
+            /* Decrement number of columns per row for lower part */
+            columns--;
+        }
+
+        /* Move to next line */
+        printf("\n");
+    }
+
+    return 0;
+}
